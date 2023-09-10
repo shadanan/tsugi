06057c2437145aa8e6f82d0083effba10d1b93e6
Shad Sharma <shadanan@gmail.com>
2023-09-09 22:40:03 -0700

    GitHub PRs and PR review tasks

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,38 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use crate::github::AuthenticatedGithubClient;
+use std::thread;
+use tauri::{async_runtime::block_on, Manager};
+
+mod github;
+mod task;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn get_tasks(client: tauri::State<'_, AuthenticatedGithubClient>) -> Result<String, String> {
+    let tasks = client.get_tasks().await;
+    Ok(serde_json::to_string(&tasks).unwrap())
+}
+
+async fn init() -> AuthenticatedGithubClient {
+    github::init().await
 }
 
 fn main() {
+    let client = block_on(init());
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(client)
+        .setup(|app| {
+            let handle = app.handle();
+            tauri::async_runtime::spawn(async move {
+                loop {
+                    handle.emit_all("tasks", "hello").unwrap();
+                    thread::sleep(std::time::Duration::from_secs(5));
+                }
+            });
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![get_tasks])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,72 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+
+import { IconGitPullRequest, IconFileUnknown } from "@tabler/icons-react";
+import { listen } from "@tauri-apps/api/event";
+
+interface Task {
+  id: string;
+  kind: string;
+  url: string;
+  title: string;
+  description: string;
+  state: string;
+  created_at: string;
+  updated_at: string;
+  closed_at: string;
+  requestor: string;
+}
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [tasks, setTasks] = useState<Task[]>([]);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  useEffect(() => {
+    (async () => {
+      const tasks: Task[] = JSON.parse(await invoke("get_tasks"));
+      setTasks(tasks);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const unlisten = await listen("tasks", (payload: any) => {
+        console.log(payload);
+      });
+    })();
+  }, []);
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Kind</th>
+          <th>Date</th>
+          <th>Requestor</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          // iterate over tasks and render them
+          tasks.map((task: any) => (
+            <tr key={task.id}>
+              <td>
+                <a href={task.url}>{task.title}</a>
+              </td>
+              <td>
+                {task.kind === "github-pull-request-review" ? (
+                  <IconGitPullRequest />
+                ) : (
+                  <IconFileUnknown />
+                )}
+              </td>
+              <td>{task.created_at}</td>
+              <td>{task.requestor}</td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
   );
 }
 
