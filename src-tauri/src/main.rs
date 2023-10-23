dc67a68064938948140bedea6ef2ed87bbe27ea8
Shad Sharma <shadanan@gmail.com>
2023-10-22 22:18:04 -0700

    Decouple UI refresh from task refresh (#12)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,82 +7,106 @@ mod plugin;
 mod storage;
 mod task;
 
-use crate::plugin::Plugin;
 use error::TsugiError;
 use github::{AuthenticatedGithubClient, GitHubPrAuthorPlugin, GitHubPrReviewPlugin};
 use std::fs;
+use std::path::PathBuf;
+use std::sync::mpsc::Sender;
+use std::thread;
+use std::time::Duration;
 use storage::Storage;
-use task::{GetTasksResponse, PluginStatus};
+use task::GetTasksResponse;
+use tauri::api::notification::Notification;
 use tauri::async_runtime::block_on;
-use tauri::Manager;
+use tauri::{App, Manager};
 
-async fn update_tasks(storage: &Storage, plugin: &Box<dyn Plugin>) -> Result<(), TsugiError> {
-    storage.update(&plugin.name(), plugin.tasks().await?)
-}
-
-async fn collect_results(storage: &Storage, plugins: &Vec<Box<dyn Plugin>>) -> GetTasksResponse {
-    let mut statuses: Vec<PluginStatus> = Vec::new();
-    // TODO: Run all plugins in parallel
-    for plugin in plugins {
-        match update_tasks(&storage, &plugin).await {
-            Ok(()) => {
-                statuses.push(PluginStatus {
-                    name: plugin.name(),
-                    status: "ok".to_string(),
-                    message: "".to_string(),
-                });
-            }
-            Err(e) => {
-                statuses.push(PluginStatus {
-                    name: plugin.name(),
-                    status: "error".to_string(),
-                    message: format!("{:?}", e),
-                });
-            }
-        }
-    }
-
-    let tasks = match storage.all_tasks() {
-        Ok(tasks) => tasks,
-        Err(e) => {
-            statuses.push(PluginStatus {
-                name: "storage".to_string(),
-                status: "error".to_string(),
-                message: format!("{:?}", e),
-            });
-            Vec::new()
-        }
-    };
-
-    GetTasksResponse { statuses, tasks }
-}
+struct AppLocalDataDir(PathBuf);
 
 #[tauri::command]
 async fn get_tasks(
-    plugins: tauri::State<'_, Vec<Box<dyn Plugin>>>,
-    storage: tauri::State<'_, Storage>,
+    data_dir: tauri::State<'_, AppLocalDataDir>,
 ) -> Result<GetTasksResponse, TsugiError> {
-    let results = collect_results(&storage, &plugins).await;
-    Ok(results)
+    let storage = Storage::new(&data_dir.0)?;
+    match storage.all_tasks() {
+        Ok(tasks) => Ok(GetTasksResponse { tasks }),
+        Err(e) => Err(e),
+    }
 }
 
-fn main() {
+fn poll(data_dir: &PathBuf, identifier: &str) -> Result<Sender<&'static str>, TsugiError> {
+    let (tx, rx) = std::sync::mpsc::channel::<&str>();
+
     let client = block_on(AuthenticatedGithubClient::new());
     let plugins = vec![
         GitHubPrReviewPlugin::new(&client),
         GitHubPrAuthorPlugin::new(&client),
     ];
 
+    let storage = Storage::new(&data_dir)?.ensure_schema()?;
+    let identifier = identifier.to_string();
+
+    thread::spawn(move || {
+        while let Ok(_) = rx.recv() {
+            println!("⏰ Updating tasks...");
+            for plugin in &plugins {
+                let name = plugin.name();
+
+                let tasks = match block_on(plugin.tasks()) {
+                    Ok(tasks) => tasks,
+                    Err(error) => {
+                        println!("🛑 Error getting tasks for plugin {name}: {error}");
+                        continue;
+                    }
+                };
+
+                match storage.update(&name, tasks) {
+                    Ok(tasks) => {
+                        for task in tasks {
+                            if let Err(e) = Notification::new(&identifier)
+                                .title(format!("New {}", name))
+                                .body(&task.title)
+                                .show()
+                            {
+                                println!("Failed to show notification: {:?}", e);
+                            }
+                        }
+                        println!("✅ Updated tasks for plugin {name}");
+                        // TODO: trigger a refresh of the UI
+                    }
+                    Err(error) => {
+                        println!("🛑 Error updating tasks for plugin {name}: {error}");
+                    }
+                }
+            }
+        }
+    });
+
+    {
+        let tx = tx.clone();
+        thread::spawn(move || loop {
+            if let Err(e) = tx.send("poll") {
+                println!("Error sending message: {:?}", e);
+            }
+            thread::sleep(Duration::from_secs(60));
+        });
+    }
+
+    Ok(tx)
+}
+
+fn setup(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
+    let data_dir = app.path_resolver().app_local_data_dir().unwrap();
+    fs::create_dir_all(&data_dir)?;
+    let identifier = &app.config().tauri.bundle.identifier;
+    app.manage(AppLocalDataDir(data_dir.clone()));
+    poll(&data_dir, &identifier)?;
+
+    Ok(())
+}
+
+fn main() {
     tauri::Builder::default()
-        .setup(|app| {
-            let data_dir = app.path_resolver().app_local_data_dir().unwrap();
-            fs::create_dir_all(&data_dir)?;
-            let identifier = &app.config().tauri.bundle.identifier;
-            let storage = Storage::new(&data_dir, identifier)?;
-            app.manage(storage);
-            Ok(())
-        })
-        .manage(plugins)
+        .setup(setup)
         .invoke_handler(tauri::generate_handler![get_tasks])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
